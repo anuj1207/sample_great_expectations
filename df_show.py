eeaf4d3c1e153e8ef6ede66e852c876d05a27359
Anuj Saxena <anuj.saxena@thoughtworks.com>
2022-11-22 12:12:30 +0530

    Modify sample script to pick the existing spark instance

--- df_show.py
+++ df_show.py
@@ -10,7 +10,6 @@ data_path = "sample-data/yellow_tripdata_sample_2019-01.csv"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    spark = create_spark_session()
     df = create_sample_df(spark)
     df.show()
     spark.stop()
